0d12315a6f779d3bf3ff700d1cf9dd9f90418b73
Dani Latorre <danilat83@gmail.com>
2011-12-14 06:04:24 +0100

    establecido el ancho máximo de las fotos

--- templates/detail.php
+++ templates/detail.php
@@ -1,6 +1,6 @@
 <b>Categoría:</b> <?php echo $issue['category']?><br/>
 <b>Descripción:</b> <?php echo $issue['description']?><br/>
 <?php if(isset($issue['photo'])){
-	echo '<img src="photos/'.$issue['photo'].'" width="400" height="250">';
+	echo '<img src="photos/'.$issue['photo'].'" width="200">';
 }?>
 <br/>
